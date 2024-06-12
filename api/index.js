ff15580d1ad237caf7a0a759eb8c97b7ef51d5c3
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-06-12 18:50:20 +0500

    all backend

---
+++ api/index.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
+import cors from 'cors'; 
+import cookieParser from 'cookie-parser'
+dotenv.config();
+
+mongoose.connect(`${process.env.MONGO}/realEstate`).then(() => {
+    console.log('Connected to MongoDB')
+}).catch((err) => {
+    console.log(err)
+})
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
+
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
+app.use("/api/listings", listingRouter)
+
+
+app.use(cors(
+    {
+        origin: ["http://localhost:5173"],
+        methods: ['POST', "GET"],
+        credentials: true
+    }
+))
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    })
+})
